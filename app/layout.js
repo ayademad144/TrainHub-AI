d1208ef4961f3cc01a570c36913e2eb14623b041
Ayad Emad <ayad.emad144@gmail.com>
2026-07-19 12:56:52 +0300

    feat: add Vercel Analytics integration to layout and update package dependencies

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { siteConfig } from "@/lib/site-config";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 import { inter, plusJakartaSans } from "./fonts";
 import NavBar from "@/components/layout/navbar";
@@ -76,6 +77,7 @@ export default function RootLayout({ children }) {
         <NavBar />
         {children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
